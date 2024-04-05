b9a655f25ba76d4106f67928fbc892baf5e568f0
Nikhil2005Rai <n2005rai@gmail.com>
2024-04-05 13:44:34 +0530

    Added javascript

---
+++ script.js
@@ -0,0 +1,17 @@
+const form = document.querySelector("form")
+
+form.addEventListener('submit',(event)=>{
+    event.preventDefault()
+    //prevent the form to submit to server
+    const height = parseInt(document.querySelector("#height").value)
+    const weight = parseInt(document.querySelector("#weight").value)
+    const bmi = weight/((height/100)**2)
+    const result = document.querySelector("#result")
+    console.log(result)
+    if (height === '' || height < 0 || isNaN(height) || weight === '' || weight < 0 || isNaN(weight)) {
+        result.innerHTML = "Please enter valid values...."
+    }
+    else{
+        result.innerHTML = `The BMI is ${bmi}`
+    }
+})

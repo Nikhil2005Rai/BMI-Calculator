b0b68d12206ebb17a012a168a49e22141b85c809
Nikhil2005Rai <n2005rai@gmail.com>
2024-04-05 19:00:04 +0530

    Decimal precision

--- script.js
+++ script.js
@@ -6,12 +6,13 @@ form.addEventListener('submit',(event)=>{
     const height = parseInt(document.querySelector("#height").value)
     const weight = parseInt(document.querySelector("#weight").value)
     const bmi = weight/((height/100)**2)
+    // console.log(bmi.toString().slice(0,5))
     const result = document.querySelector("#result")
-    console.log(result)
+    // console.log(result)
     if (height === '' || height < 0 || isNaN(height) || weight === '' || weight < 0 || isNaN(weight)) {
         result.innerHTML = "Please enter valid values...."
     }
     else{
-        result.innerHTML = `The BMI is ${bmi}`
+        result.innerHTML = `The BMI is ${bmi.toString().slice(0,5)}`
     }
 })
